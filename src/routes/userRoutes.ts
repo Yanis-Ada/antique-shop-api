e84bba211bdeaff84cb222f2cddd906a3b4d85b8
Yanis <yanis.blanchard@gmail.com>
2025-11-13 16:38:24 +0100

    CRUD User: add updateUser route and controller

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.post('/users', UserController.createUser);
 router.get("/users", UserController.getAllUsers);
 router.get('/users/:id', UserController.getUserById);
+router.patch("/users/:id", UserController.updateUser);
 
 export default router;
